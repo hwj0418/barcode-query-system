4d548d3170cc95770fd00b042ee4fa6d40dc38eb
Wj <ddh0418@gmail.com>
2019-08-29 17:48:00 +0800

    setup Router for App.jsx
    Setup a router for home page, create a empty home page.

---
+++ barcode-query-system/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+
+    render(){
+        return(
+            <div>This is Home page, where users first see.</div>
+        )
+    }
+}
+
+export default Home;
