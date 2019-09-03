97ebf5a7aad97bfd4d0f0c4557d570d6faca411d
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2019-09-03 01:45:45 -0400

    added media files necessary so far, splited ScanScreen from App.js

--- rn-barcode-query-system/App.js
+++ rn-barcode-query-system/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import BarcodeScanner from './components/BarcodeScanner.js';
+// Import screens
+import ScanScreen from './screens/ScanScreen.js';
 
 export default function App() {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.barCodeScannerContainer}>
-        <BarcodeScanner />
-      </View>
+      <ScanScreen />
     </View>
   );
 }
@@ -19,11 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  barCodeScannerContainer: {
-    width: 300,
-    maxWidth: '80%',
-    height: 500,
-    maxHeight: '80%'
   }
 });
---
+++ rn-barcode-query-system/screens/ScanScreen.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
+
+// Import components
+import BarcodeScanner from '../components/BarcodeScanner.js'
+
+// Import constants
+import Colors from '../constants/colors.js';
+import Texts from '../constants/texts-en.js';
+
+const ScanScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.barCodeScannerContainer}>
+        <BarcodeScanner />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  barCodeScannerContainer: {
+    width: 300,
+    maxWidth: '80%',
+    height: 500,
+    maxHeight: '80%'
+  }
+});
+
+export default ScanScreen;
